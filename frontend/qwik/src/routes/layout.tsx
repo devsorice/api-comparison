6a57a264595655b0a69483583e0aed8b9a25cb3d
Francesco <devsorice@gmail.com>
2024-07-26 16:21:34 +0900

    removed call

--- frontend/qwik/src/routes/layout.tsx
+++ frontend/qwik/src/routes/layout.tsx
@@ -1,7 +1,6 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
-export { useUsersLoader } from './dashboard/users';
 
 import Header from "../components/starter/header/header";
 import Footer from "../components/starter/footer/footer";
