5a1714ec9dd0b9637fa1ea1dadfcdadef29bf9f5
Francesco <devsorice@gmail.com>
2024-08-01 01:06:23 +0900

    added js

---
+++ generator/generating/frontend/frameworks/html/js/sidebar.js
@@ -0,0 +1,4 @@
+const triggers = {}
+const click = fn => document.addEventListener('click', fn)
+click(e => Object.keys(triggers).forEach(x => e.target.matches(x) || e.target.closest(x) ? triggers[x](e) : null))
+triggers['aside .parent>a'] = e => console.log(e)
